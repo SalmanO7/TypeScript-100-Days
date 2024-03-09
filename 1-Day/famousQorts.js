5e0b9d08c0de6b78c23c3eb863165286df5632fe
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-03-10 01:46:38 +0500

    Add files via upload

---
+++ 1-Day/famousQorts.js
@@ -0,0 +1,5 @@
+console.log("Cristiano Ronaldo said,\" telent without work is nothing\"");
+//Use Variable
+var auther = "Cristiano Ronaldo";
+console.log("".concat(auther, " said,\" telent without work is nothing\""));
+console.log("Salman");
---
+++ 1-Day/famousQorts.ts
@@ -0,0 +1,5 @@
+console.log(`Cristiano Ronaldo said," telent without work is nothing"`);
+//Use Variable
+let uauther:string = "Cristiano Ronaldo"
+console.log(`${uauther} said," telent without work is nothing"`)
+console.log("Salman")
