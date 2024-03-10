dcc6d966fd687748181bff5ef77827288224d85d
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-03-11 02:15:56 +0500

    Add files via upload

---
+++ 2-Day/days02.js
@@ -0,0 +1,7 @@
+var nmbr1 = 16;
+var nmbr2 = 10;
+var sum = nmbr1 + nmbr2;
+var multi = nmbr1 * nmbr2;
+var sub = nmbr1 - nmbr2;
+var divid = nmbr1 / nmbr2;
+console.log("sum value is = ".concat(sum, ";  Multiply is =").concat(multi, "; subraction is ").concat(sub, "; and divide is ").concat(divid));
---
+++ 2-Day/days02.ts
@@ -0,0 +1,10 @@
+// Airthemathic Operators
+let nmbr1 = 16;
+let nmbr2 = 10;
+
+let sum = nmbr1 + nmbr2;
+let multi = nmbr1 * nmbr2;
+let sub = nmbr1 - nmbr2;
+let divid = nmbr1 / nmbr2;
+
+console.log(`sum value is = ${sum};  Multiply is =${multi}; subraction is ${sub}; and divide is ${divid}`);
