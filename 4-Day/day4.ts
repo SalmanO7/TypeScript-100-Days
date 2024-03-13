d3ab26fd8c666ab420d244101666de94a270c1fc
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-03-14 01:20:16 +0500

    Add files via upload

---
+++ 4-Day/day4.ts
@@ -0,0 +1,7 @@
+let car = "marsedees"
+console.log("Is car == 'marsedees'? predict True.")
+console.log(car == 'marsedees')
+console.log("Is car == 'bmw'? predict false.")
+console.log(car == 'bmw')
+console.log("Is car == 'tyota'? predict True.")
+console.log(car == 'tyota')
