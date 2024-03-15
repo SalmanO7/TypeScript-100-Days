ef11da0758f4677ba04ef1bf90fca49d3f74a424
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-03-16 02:39:08 +0500

    Add files via upload

---
+++ 6-Day/day6.js
@@ -0,0 +1,10 @@
+//Objects In typescript
+let book = {
+    title: "The Hobbit",
+    author: "J.R.R Tolkien",
+    yearPublished: 1937
+};
+console.log(`${book.title}`);
+console.log(`${book.author}`);
+console.log(`${book.yearPublished}`);
+export {};
---
+++ 6-Day/day6.ts
@@ -0,0 +1,10 @@
+//Objects In typescript
+
+let book: { title: string, author: string, yearPublished: number } = {
+    title: "The Hobbit",
+    author: "J.R.R Tolkien",
+    yearPublished: 1937
+};
+console.log(`${book.title}`);
+console.log(`${book.author}`);
+console.log(`${book.yearPublished}`);
