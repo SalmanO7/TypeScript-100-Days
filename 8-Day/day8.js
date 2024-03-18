b50963f32f21a44db0e7bad529c9b452fd9bc5f8
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-03-18 08:44:32 +0500

    Add files via upload

---
+++ 8-Day/day8.js
@@ -0,0 +1,16 @@
+// Equality Test with String
+console.log("Testing Equality with String");
+console.log("apple" == "apple");
+// console.log("banana" == "Banana");
+// using the lower case function
+console.log("Testing With Lowercase");
+console.log("Apple".toLocaleLowerCase() == "apple");
+// Numaric Testing
+console.log("Numarical Test:");
+console.log(10 > 5);
+console.log(2 < 1);
+// Testing using "and" and "or" Operators
+console.log("Testing using 'and' and 'or' Operators");
+console.log(true && false);
+console.log(true || false);
+export {};
