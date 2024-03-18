e8517faf8096751de474bff59f14f39a289d8979
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-03-19 02:57:39 +0500

    Add files via upload

---
+++ 9-Day/day9.ts
@@ -0,0 +1,14 @@
+let age = 9;
+if (age < 2) {
+    console.log("The perso is baby")
+} else if (age < 4) {
+    console.log("The person is todler")
+} else if (age < 13) {
+    console.log("The person is kid")
+} else if (age < 20) {
+    console.log("The person is teenager")
+} else if (age < 65) {
+    console.log("The person is adult")
+} else {
+    console.log("The person is elder")
+}
