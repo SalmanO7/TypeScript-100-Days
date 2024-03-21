dfbd98de4b91314656b2db981fd1a4e2e8a1080a
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-03-21 06:37:29 +0500

    Add files via upload

---
+++ 11-Day/11-Question.js
@@ -0,0 +1,7 @@
+//Greetins: Start with the array you used in Exresice 11, but instead of just printing each should be the same,
+// But each message should be personalized with the person's name.
+var listOfArray = ["Salman", "Faizan", "Asif"];
+for (var _i = 0, listOfArray_1 = listOfArray; _i < listOfArray_1.length; _i++) {
+    var listArray = listOfArray_1[_i];
+    console.log("Hey Good Moring ".concat(listArray));
+}
---
+++ 11-Day/11-Question.ts
@@ -0,0 +1,8 @@
+//Greetins: Start with the array you used in Exresice 11, but instead of just printing each should be the same,
+// But each message should be personalized with the person's name.
+
+let listOfArray: string[] = ["Salman", "Faizan", "Asif"];
+
+for (const listArray of listOfArray) {
+    console.log(`Hey Good Moring ${listArray}`);
+}
