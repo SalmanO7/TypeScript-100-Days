6f5843a2077a2fc287ceff7eb22a2441c9c5fbf0
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-03-27 14:23:12 +0500

    Add files via upload

---
+++ 16-Day/day16.js
@@ -0,0 +1,17 @@
+//4. Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+function makeSandwich(...items) {
+    console.log("Sandwish Summary");
+    if (items.length === 0) {
+        console.log("No Items Selected");
+    }
+    else {
+        items.forEach((item, index) => {
+            console.log(`Item ${index + 1}: ${item}`);
+        });
+        console.log("Enjoy Your Sandwish ");
+    }
+}
+makeSandwich("Cheese", "Tomato", "Lettuce");
+makeSandwich("Chicken", "Mayonaise");
+makeSandwich();
+export {};
