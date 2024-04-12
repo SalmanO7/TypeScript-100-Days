4af51c84632f5f27c3c8bc648810a69490145edb
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-04-12 20:37:54 +0500

    Add files via upload

---
+++ 22-Day/day22.js
@@ -0,0 +1,18 @@
+// Filling in the blueprint with an example student information
+var student = {
+    name: 'Salman',
+    age: 18,
+    courses: ["Math", "English", "Urdu", "Physics"]
+};
+// Showing the student's information
+console.log(student);
+//Descring a circles using our shapes type
+var rectangle = {
+    kind: 'Circle',
+    width: 10,
+    radius: 20,
+    height: 20,
+};
+//Showing what we described
+console.log(rectangle);
+// we made a flexible program that can decribe different shapes in details.
