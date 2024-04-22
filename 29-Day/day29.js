b61d1e1413eee8d6a67e979d564587ff447d83b4
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-04-22 14:46:55 +0500

    Add files via upload

---
+++ 29-Day/day29.js
@@ -0,0 +1,15 @@
+// 1 Function Declaration for squaring a number
+function squarDeclaration(nmbr) {
+    return nmbr * nmbr;
+}
+console.log(squarDeclaration(5));
+// 2 Function Experassion for squaring a number
+var squarExpression = function (nmbr) {
+    return nmbr * nmbr;
+};
+console.log(squarExpression(10));
+// 3 Function Arrows for squaring a number
+var squarArrow = function (nmbr) {
+    return nmbr * nmbr;
+};
+console.log(squarArrow(15));
