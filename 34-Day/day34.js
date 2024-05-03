997dddb83de1504a30818c95700886a5ef9a723e
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-05-03 14:22:32 +0500

    Add files via upload

---
+++ 34-Day/day34.js
@@ -0,0 +1,19 @@
+// this function creates a random number between 1 and 10
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+//Example: Getting a random number 
+console.log(generateRandomNumber());
+// 2 Defines an Array with three favorite fruits
+var FavoriteFruits = ["Apple", "Orange", "Banana"];
+FavoriteFruits.push("Mango");
+// Add 1 more favorite Fruit in this array
+console.log(FavoriteFruits);
+// 3 this Function Remove  the last element from the array and returns it
+function removeLastElement(arr) {
+    return arr.pop();
+}
+//Example Removing the last fruit from the array
+var fruits = ["Apple", "Orange", "Banana"];
+console.log(removeLastElement(fruits)); // The OUtput is Banana
+console.log(fruits);
