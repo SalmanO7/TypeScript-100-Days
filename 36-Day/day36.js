f3df816f543c98c562eaa0527706b8a9a6b1ec65
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-05-14 00:56:09 +0500

    Add files via upload

---
+++ 36-Day/day36.js
@@ -0,0 +1,11 @@
+// #1 Finds the square root of 144
+var squareRoot = Math.sqrt(144);
+console.log("The square root of 144 is ".concat(squareRoot, "."));
+// #2 Generates a random integer b/w 1 and 10
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(generateRandomNumber());
+// #3 Calculates the absolute difference between -5 and 5
+var absoluteDifference = Math.abs(-5 - 5);
+console.log("The absolute difference between -5 and 5 is: ".concat(absoluteDifference, "."));
