c08ba80a64f6e200296a27772409e9d75d7bac36
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-06-01 20:48:06 +0500

    Add files via upload

---
+++ 40-Day/day40.ts
@@ -0,0 +1,24 @@
+// This loop logs numbers from 1 to 10
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+// Each Iteration increase the value of 1 by 1, logging it until it reaches 10.
+
+// #2  initialization a counter
+let count: number = 0
+
+while (count < 5) {
+    console.log("salman")
+    count++
+}
+// Reapts logging "salman" 5 times
+
+// #3 Define an array of favorite movies
+const favoriteMovies: string[] = ["Mission Impossible", "The Hobbit", "The Load Of the Rings", "Man of the steel"]
+
+// uses a for ...of loop to iterate through favoriteMovie
+for (const movie of favoriteMovies) {
+    console.log(movie)
+}
+//Each movie in the array is logged to the console.
