e96a3dac5f7155fa4ce6b2e6ba050b318d7262ee
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-06-03 23:36:13 +0500

    Add files via upload

---
+++ 42-Day/day42.js
@@ -0,0 +1,29 @@
+// traditional function expression
+let add = function (x, y) {
+    return x + y;
+};
+// convert In Arrow Function
+let add2 = (x, y) => {
+    return x + y;
+};
+console.log(add(1, 2));
+console.log(add2(1, 2));
+// 2# Arrow function that calculates the product of all its perameters
+const multiplyPerameters = (...numbers) => {
+    numbers.reduce((total, number) => total * number, 1);
+};
+console.log(multiplyPerameters(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14));
+// this arrow function sum there arrays numbers
+// 3# Demonstrating 'this' in traditional vs. arrow functions
+const traditional = {
+    value: "traditionalVsArrow Value",
+    traditionalFunction: function () {
+        console.log("Traditional Function :", this.value);
+        const arrowFunction = () => {
+            console.log("Arrow Function :", this.value);
+        };
+        arrowFunction();
+    },
+};
+traditional.traditionalFunction();
+export {};
