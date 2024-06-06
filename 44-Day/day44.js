39b0397df372f1ed0bb0c1e9ce5ff5a69cc9748e
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-06-06 23:38:34 +0500

    Add files via upload

---
+++ 44-Day/day44.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+// in file day44
+var add = function (a, b) { return a + b; };
+exports.add = add;
+// this line exports an add function from day44.ts
