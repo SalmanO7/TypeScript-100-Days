b9d346a0a616d7114e05211c9a9d2b68f2339532
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-06-12 13:27:11 +0500

    Update day44.ts

--- 44-Day/day44.ts
+++ 44-Day/day44.ts
@@ -1,4 +1,30 @@
-// in file day44
-export const add = (a: number, b: number) => a + b;
+// REserved word: let - used to declare a block-scoped local variable.
+let count = 5;
 
-// this line exports an add function from day44.ts
+// Reserved word: if - used to execute a block of code of a specified condition is ture
+if (count > 0) {
+    console.log("Count is greter than 0")
+}
+
+// Reserved word: return - used to exit a function and return a value from that function
+function add(a: number, b: number) {
+    return a + b;
+}
+// Demonstrates valid use cases for the reserved words 'let' , 'if' and 'return'.
+
+// 2 Attempting to use a reserved word as a variable name;
+
+// let if = 5; // This line is syntax error coz if is a resvered keyword
+console.log(
+    "Using a reserved word as a variable name causes a syntax error in javascript where"
+)
+
+//3 Example use of the 'await' reserved word in asynchronous javascript
+async function fetchData() {
+    // const data = await fetchSomething();   // Error in this line it's syntex error  
+    // console.log(data)
+    //The 'await' keyword pauses the escution untill the promise settles, and then resume with resolved value.
+}
+console.log(
+    "The 'await' keyword allows asynchronous, promise-based behavior to be written in a cleaner, more linear fashion."
+)
