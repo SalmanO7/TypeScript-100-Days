ffee2414de4ca322ecaaa86b26c14a45db12d8c4
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-07-01 00:27:48 +0500

    Add files via upload

---
+++ 49-Day/day49.js
@@ -0,0 +1,13 @@
+// 1 Synchronous example
+console.log("Before Synchronous operation");
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+console.log("After Synchronous operation");
+// 2 Asynchronous Example 
+console.log("Before Asynchronous operation");
+setTimeout(() => {
+    console.log("Asynchronous operation completed");
+}, 1000);
+console.log("After Synchronous operation setup");
+export {};
