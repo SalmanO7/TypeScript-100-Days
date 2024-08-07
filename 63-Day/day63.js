49032e9c354f802a75c44da88d249cfc538b4e51
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-08-07 17:53:41 +0500

    Add files via upload

---
+++ 63-Day/day63.js
@@ -0,0 +1,20 @@
+// TYPE ANNOTATION
+let myNumberExOne = 24;
+let mySTringExOne = "Hellow, Typescript";
+console.log("My Number is :", myNumberExOne);
+console.log("My String is :", mySTringExOne);
+// Another way to write this code is as follows:
+let myNumberExTwo;
+let mySTringExTwo;
+myNumberExTwo = 50;
+mySTringExTwo = "Hellow World";
+console.log("My Number is :", myNumberExTwo);
+console.log("My String is :", mySTringExTwo);
+// DYNAMIC TYPE DETERMINATION
+let greeting = "Hello World";
+console.log(greeting);
+// ANY TYPE
+let myVariable = 19;
+myVariable = "Hello World";
+console.log(myVariable);
+export {};
