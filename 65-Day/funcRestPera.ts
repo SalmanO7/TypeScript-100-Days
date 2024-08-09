1a7b07cd7fa06b4d18312e2a16b065a5e42bffbc
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-08-10 00:56:57 +0500

    Add files via upload

---
+++ 65-Day/funcRestPera.ts
@@ -0,0 +1,20 @@
+//  ****Function Rest Parameter****
+
+// Function with a rest parameter and type annotations
+function addAll(...nums: number[]): number {
+    let result = 0;
+    for (let i = 0; i < nums.length; i++) {
+        result += nums[i];
+    }
+    return result;
+}
+
+// Calling the function with various numeric arguments
+console.log(addAll(10, 20, 30, 100, 10.5, +true)); // Output: 170.5
+
+// In this code:
+// - The `addAll` function is defined with a rest parameter `...nums`, which captures a variable number of numeric arguments and stores them in an array named `nums`. The type annotation `number[]` specifies that the elements in the `nums` array should be of type `number`.
+// - Inside the function, a `for` loop is used to iterate through the `nums` array and calculate the sum of all the numbers provided as arguments.
+
+// pro tip
+// **`+true`** in JavaScript is a unary plus operator (**`+`**) applied to the boolean **`true`**, resulting in the numeric value **`1`**.
