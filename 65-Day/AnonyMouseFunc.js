a89f070091b4aa049515baed988266af6ea34b22
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-08-10 00:54:26 +0500

    Add files via upload

---
+++ 65-Day/AnonyMouseFunc.js
@@ -0,0 +1,8 @@
+"use strict";
+//  Anonymous Function
+// An anonymous function in TypeScript (and in JavaScript) is a function that doesn't have a name. Instead of declaring a named function, you define the function directly as an expression. Anonymous functions are often used in various scenarios, including passing functions as arguments to other functions or creating functions on the fly. They are particularly useful when you need a small, one-off function without polluting the global scope with a function name.
+const add = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(add(10, 20)); // Output: 30
+// - The `add` variable is assigned an anonymous function that adds two numbers, `num1` and `num2`.
