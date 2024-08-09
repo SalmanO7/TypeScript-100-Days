13e677a288ed6d221d5a272cbecc140f2aad4556
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-08-10 00:55:49 +0500

    Add files via upload

---
+++ 65-Day/ArrowFunction.ts
@@ -0,0 +1,9 @@
+//  Arrow Function
+
+// An arrow function in TypeScript is a concise way to define functions using a more compact syntax compared to traditional function expressions. Arrow functions are also sometimes called "fat arrow" functions due to the use of the **`=>`** syntax. They were introduced in ECMAScript 6 (ES6) and have since become a popular way to write functions in JavaScript and TypeScript.
+
+const addWithArrow = (num1: number, num2: number): number => num1 + num2;
+console.log(addWithArrow(10, 20)); // Output: 30
+
+// - The `addWithArrow` variable is assigned an arrow function that performs the same addition operation.
+
