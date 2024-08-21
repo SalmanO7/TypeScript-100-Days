446d947a384776797d7901df11878092004b9a3b
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-08-21 15:33:30 +0500

    Tuples Examples

---
+++ 68-Day/Tuple/tuple1.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Defining a tuple type
+let article = [11, "Title One", true];
+// Assigning a new tuple value to the `article` variable
+article = [12, "Title Two", false];
+// Trying to push a new element into a tuple (will result in an error)
+article.push(100);
+console.log(article);
+// Destructuring the tuple into individual variables
+const [id, title, published] = article;
+console.log(id);
+console.log(title);
+console.log(published);
