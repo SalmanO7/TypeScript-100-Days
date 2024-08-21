a7d82c1dcdd6267374b26f44b26b7b5ecb72b602
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-08-22 02:37:41 +0500

    Update Enums.js

--- 69-Day/Enums.js
+++ 69-Day/Enums.js
@@ -1,1 +1,15 @@
+ * ENUMS *
+var Days;
+(function (Days) {
+    Days[Days["Sunday"] = 0] = "Sunday";
+    Days[Days["Monday"] = 1] = "Monday";
+    Days[Days["Tuesday"] = 2] = "Tuesday";
+    Days[Days["Wednesday"] = 3] = "Wednesday";
+    Days[Days["Thursday"] = 4] = "Thursday";
+    Days[Days["Friday"] = 5] = "Friday";
+    Days[Days["Saturday"] = 6] = "Saturday";
+})(Days || (Days = {}));
+const today = Days.Wednesday;
+console.log(`Today is ${Days[today]}`);
+export {};
 
