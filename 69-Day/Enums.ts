b5d088658a2375f05551ecab17372abe2d7a42ac
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-08-22 02:36:21 +0500

    Update Enums.ts

--- 69-Day/Enums.ts
+++ 69-Day/Enums.ts
@@ -1,1 +1,15 @@
+ * ENUMS *
 
+enum Days {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+  }
+  
+  const today: Days = Days.Wednesday;
+  
+  console.log(`Today is ${Days[today]}`);
